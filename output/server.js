65d959e6f6d272330a95c5c1fa1810692301f85b
Abirk2day <abirk2day@gmail.com>
2021-06-19 08:15:42 +0600

    typescript

---
+++ output/server.js
@@ -0,0 +1,20 @@
+"use strict";
+var a;
+a = " abir";
+console.log(a);
+// object properties
+var c;
+c = {
+    name: "abir ",
+    age: 445,
+    email: "abirk2day@gamil.com",
+    isSingle: true
+};
+console.log(c);
+var d;
+d = {
+    name: "ano",
+    age: 55,
+    rooot: true
+};
+console.log(d);
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+let a: string | number;
+
+a = " abir";
+console.log(a);
+
+
+// object properties
+let c: {
+    name: string;
+    age: number;
+    email: string;
+    isSingle: boolean;
+};
+
+  c= {
+      name: "abir ",
+      age: 445,
+      email: "abirk2day@gamil.com",
+      isSingle: true
+        
+}
+console.log(c);
+
+let d: object;
+
+d = {
+    name: "ano",
+    age: 55,
+    rooot: true
+}
+
+console.log(d);
+
